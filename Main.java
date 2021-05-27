e46a93967fe7145a0af95cdebb67c4e4567c3072
jennymar <56415672+jennymar@users.noreply.github.com>
2021-05-27 14:07:15 -0700

    updated Block.java

--- Main.java
+++ Main.java
@@ -12,5 +12,12 @@ public class Main {
             System.out.println("x: " + p.getX() + "; y: " + p.getY());
         }
 
+        Block blockTest = new Block(pieceTest, true);
+        Block blockRandom = Block.getRandomBlock();
+
+        System.out.println("initial points: " + blockTest.getPointsStr());
+        blockTest.rotate();
+        System.out.println("rotated points: " + blockTest.getPointsStr());
+
     }
 }
